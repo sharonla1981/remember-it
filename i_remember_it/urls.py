8dc3365a06ab12945f4bc63f5cd82da9c7f8f443
Sharon <sharon@asanarebel.com>
2018-08-04 00:53:00 +0200

    add privacy policy

--- i_remember_it/urls.py
+++ i_remember_it/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
-from allauth.account.views import SignupView, LoginView, PasswordResetView
+from allauth.account.views import SignupView, LoginView, PasswordResetView,TemplateView
 
 class MySignupView(SignupView):
     template_name = 'signup.html'
@@ -11,6 +11,9 @@ class MyLoginView(LoginView):
 class MyPasswordResetView(PasswordResetView):
     template_name = 'password_reset.html'
 
+class PrivacyPolicy(TemplateView):
+    template_name = 'privacy-policy.html'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('words/',include('words.urls')),
@@ -19,5 +22,7 @@ urlpatterns = [
     path('myprofile/',include('myprofile.urls')),
     path('accounts/login/',MyLoginView.as_view(),name='account_login'),
     path('accounts/',include('allauth.urls')),
+    path('privacy_policy/',PrivacyPolicy.as_view(),name='privacy_policy'),
+
 
 ]
